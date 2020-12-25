4a15843c81e26a72a5f5bbfe5bc776d57cbb88a9
Yash786-rgb <yagrawal@ch.iitr.ac.in>
2020-12-25 15:59:32 +0530

    Updates

---
+++ precache-manifest.5a7872aeafe7c6b8c72dec01d933bf7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d979d597c0ea885e6d19540cda12491",
+    "url": "/RGB-Color-Game/index.html"
+  },
+  {
+    "revision": "5b0e645cb78ab6687459",
+    "url": "/RGB-Color-Game/static/js/2.8d70d023.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RGB-Color-Game/static/js/2.8d70d023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "141d8466c2bf753a635f",
+    "url": "/RGB-Color-Game/static/js/main.2a279934.chunk.js"
+  },
+  {
+    "revision": "0e5b879456809554518e",
+    "url": "/RGB-Color-Game/static/js/runtime-main.0ef4909f.js"
+  }
+]);
